4633ca3fcae699228445d2abe1c50eed59c264be
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-04-16 00:35:28 -0300

    se borro el main y sus estilos. Se aplico la fuente principal y se modificaron algunos estilos en general

--- site_organicom/public/js/detalleProducto.js
+++ site_organicom/public/js/detalleProducto.js
@@ -4,6 +4,9 @@ const btnFavorito = document.querySelector('.favorito-contenedor p.favorito');
 const iconoFavorito = document.querySelector(
    '.favorito-contenedor p.favorito .icono'
 );
+const cardIconosFav = document.querySelectorAll(
+   '.recomendados .card-header .favorito'
+);
 
 btnFavorito.addEventListener('click', () => {
    iconoFavorito.classList.toggle('active');
@@ -14,4 +17,10 @@ btnFavorito.addEventListener('click', () => {
       btnFavorito.querySelector('span:first-child').textContent =
          'Agregar a favoritos';
    }
+});
+
+cardIconosFav.forEach((elm) => {
+   elm.addEventListener('click', () => {
+      elm.classList.toggle('active');
+   });
 });
