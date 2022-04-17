6111cad27941e6f66527abc67b5ab7f2b9bc35e3
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-04-17 16:12:14 -0300

    se termino el 2do diseño del carrito en cada viste responsive

--- site_organicom/public/js/carrito.js
+++ site_organicom/public/js/carrito.js
@@ -1,1 +1,34 @@
 // ------------
+
+const btnMostrarResumen = document.querySelector(
+   '.resumen-compra .ver-ocultar'
+);
+const cuerpoResumen = document.querySelector('.resumen-compra .cuerpo');
+const cuerpoResumenHeight = document.querySelector(
+   '.resumen-compra .cuerpo .cuerpo-height'
+);
+const cuerpoHeight =
+   cuerpoResumenHeight.clientHeight + cuerpoResumenHeight.style.paddingTop;
+
+btnMostrarResumen.addEventListener('click', () => {
+   if (!cuerpoResumen.style.height) {
+      cuerpoResumen.style.height = `${cuerpoHeight}px`;
+      btnMostrarResumen.textContent = 'Ocultar resúmen';
+      btnMostrarResumen.classList.add('show');
+   } else {
+      cuerpoResumen.style.height = '';
+      btnMostrarResumen.textContent = 'Ver resúmen';
+      btnMostrarResumen.classList.remove('show');
+   }
+});
+
+// ------------
+
+const btnEditarProductos = document.querySelector('.btn-editar-productos');
+
+btnEditarProductos.addEventListener('click', () => {
+   document.body.classList.add('modal-open');
+   carritoLateralContenedor.classList.add('show');
+   modalBg.style.display = 'block';
+   modalBg.style.position = 'fixed';
+});
