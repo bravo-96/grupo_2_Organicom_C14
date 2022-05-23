8c893687f97619cd6337009d372f192cbe594b4c
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-05-23 20:48:54 -0300

    rutas parametrizadas

--- site_organicom/controllers/productController.js
+++ site_organicom/controllers/productController.js
@@ -1,6 +1,8 @@
 module.exports = {
    detalleProducto: (req, res, next) => {
+
       return res.render('detalleProducto');
+      // return res.send(req.params);
    },
    carrito: (req, res, next) => {
       return res.render('carrito');
