4df91e4e95af96e0a0ef3f7f8aac74747afdc291
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-05-27 00:33:01 -0300

    se agrego rutas parametrizadas para los productos

--- site_organicom/controllers/productController.js
+++ site_organicom/controllers/productController.js
@@ -1,7 +1,14 @@
+const productos = require('../controllers/data/productos.json');
+
 module.exports = {
    detalleProducto: (req, res, next) => {
+      const producto = productos.find((prod) => {
+         return prod.id === +req.params.id;
+      });
 
-      return res.render('detalleProducto');
+      return res.render('detalleProducto', {
+         producto,
+      });
       // return res.send(req.params);
    },
    carrito: (req, res, next) => {
--- site_organicom/routes/products.js
+++ site_organicom/routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {detalleProducto,carrito} = require('../controllers/productController');
 
-router.get('/detalleProducto', detalleProducto);
+router.get('/detalleProducto/:id', detalleProducto);
 // router.get('/detalleProducto/:id', detalleProducto);
 router.get('/carrito', carrito);
 
