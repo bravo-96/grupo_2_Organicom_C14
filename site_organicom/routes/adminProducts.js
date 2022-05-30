1eaa13c63d3c0eed4d1bdf20f7d91b806c715ae2
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-05-29 21:55:31 -0300

    se agrego estilo al formulario de agregar producto

--- site_organicom/routes/adminProducts.js
+++ site_organicom/routes/adminProducts.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+// MULTER
+const multer = require('multer');
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, '../public/imgs/products_images');
+   },
+   filename: function (req, file, cb) {
+      cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+   },
+});
+const uploadFile = multer({ storage });
+//--------------------------------------
+
 //Si quieren cambiar nombres haganlo pero AVISEN --Alex <3
 
-let {adminProducts, agregarProducto, create, editarProducto, update} = require ("../controllers/adminController")
+let {
+   adminProducts,
+   agregarProducto,
+   create,
+   editarProducto,
+   update,
+} = require('../controllers/adminController');
 
 //POR FALLAS, MIRAR EN CONTROLLERS
 
-router.get("/",adminProducts);
+router.get('/', adminProducts);
 
 /* GET muestra el formulario */
-router.get("/agregar",agregarProducto)
+router.get('/agregar', agregarProducto);
 
 /* POST carga los datos al formulario */
-router.post("/agregarProducto",create)
+// uso de multer en el formulario
+router.post('/agregarProducto', uploadFile.single("idDelInputDeSubida") ,create);
 
-
-router.get("/editar/:id", editarProducto )
+router.get('/editar/:id', editarProducto);
 /* PUT actualiza los datos */
-router.put("/editarProducto/:id", update);
+router.put('/editarProducto/:id', update);
 
-
-
-module.exports = router
+module.exports = router;
