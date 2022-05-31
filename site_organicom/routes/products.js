77503b6e00a6813d9bf0dcbec7521ab9443a38b3
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-05-31 20:38:40 -0300

    se creo la ruta para la vista dinamica

--- site_organicom/routes/products.js
+++ site_organicom/routes/products.js
@@ -4,19 +4,18 @@ const router = express.Router();
 const {
    detalleProducto,
    carrito,
-   catFideosCereales,
-   catFrutosSecos,
-   catInfusiones,
-   catJugosBebidas,
+   categoria,
 } = require('../controllers/productController');
 
 router.get('/detalleProducto/:id', detalleProducto);
 // router.get('/detalleProducto/:id', detalleProducto);
 router.get('/carrito', carrito);
 
-router.get('/categorias/fideos_cereales', catFideosCereales);
-router.get('/categorias/frutos_secos', catFrutosSecos);
-router.get('/categorias/infusiones', catInfusiones);
-router.get('/categorias/jugos_bebidas', catJugosBebidas);
+router.get('/categorias/:categoria', categoria);
+
+// router.get('/categorias/fideos_cereales', catFideosCereales);
+// router.get('/categorias/frutos_secos', catFrutosSecos);
+// router.get('/categorias/infusiones', catInfusiones);
+// router.get('/categorias/jugos_bebidas', catJugosBebidas);
 
 module.exports = router;
