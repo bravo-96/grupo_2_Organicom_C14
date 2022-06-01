3e3ee575b14022349dfee4333aab1be341130f1b
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-06-01 00:22:31 -0300

    se aplicaron los sliders de populares y ofertas al home

--- site_organicom/controllers/indexController.js
+++ site_organicom/controllers/indexController.js
@@ -1,5 +1,7 @@
+const productos = require('../controllers/data/productos.json');
+
 module.exports = {
    index: (req, res, next) => {
-      return res.render('index', { title: 'Organicom' });
+      return res.render('index', { title: 'Organicom', productos });
    },
 };
--- site_organicom/public/js/home.js
+++ site_organicom/public/js/home.js
@@ -1,49 +1,47 @@
-let btn = document.getElementById('animate').addEventListener('click', (e) => {
-   document.body.classList.toggle('animate');
-});
-
-const swiper = new Swiper('.carousel1', {
-   // Optional parameters
-
-   // If we need pagination
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-   },
-
-   // Navigation arrows
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-   },
-
-   breakoints: {
-      640: {},
-   },
-   // And if we need scrollbar
-   // scrollbar: {
-   //   el: '.swiper-scrollbar',
-   // },
-});
-
-const slide2 = new Swiper('.carousel2', {
-   slidesPerView: 2,
-   centeredSlides: true,
-   spaceBetween: 30,
-
-   pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-      clickable: true,
-   },
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-   },
-
-   breakpoints: {
-      640: {
-         slidesPerView: 4,
+document.addEventListener('DOMContentLoaded', function () {
+   new Splide('#popularesSlider', {
+      breakpoints: {
+         450: {
+            perPage: 1,
+         },
+         600: {
+            perPage: 2,
+         },
+         800: {
+            perPage: 3,
+            gap: '1rem',
+         },
+         950: {
+            perPage: 4,
+         },
       },
-   },
+      perPage: 5,
+      pagination: false,
+      arrowPath: '0',
+      gap: '2rem',
+      padding: '0.5rem',
+   }).mount();
+
+   new Splide('#ofertasSlider', {
+      breakpoints: {
+         450: {
+            perPage: 1,
+         },
+         600: {
+            perPage: 2,
+         },
+         800: {
+            perPage: 3,
+            gap: '1rem',
+         },
+         950: {
+            perPage: 4,
+         },
+      },
+      perPage: 5,
+      pagination: false,
+      arrowPath: '0',
+      gap: '2rem',
+      padding: '0.5rem',
+   }).mount();
 });
