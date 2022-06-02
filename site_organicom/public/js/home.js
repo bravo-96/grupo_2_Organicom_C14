3a9b4fe41e227e156d663b701dfe81d9c6cd9e4c
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-06-01 23:16:41 -0300

    se termino el diseño y responsive del home, falta el slider de los banner

--- site_organicom/public/js/home.js
+++ site_organicom/public/js/home.js
@@ -44,4 +44,50 @@ document.addEventListener('DOMContentLoaded', function () {
       gap: '2rem',
       padding: '0.5rem',
    }).mount();
+
+   new Splide('#frutosSecosSlider', {
+      breakpoints: {
+         450: {
+            perPage: 1,
+         },
+         600: {
+            perPage: 2,
+         },
+         800: {
+            perPage: 3,
+            gap: '1rem',
+         },
+         950: {
+            perPage: 4,
+         },
+      },
+      perPage: 5,
+      pagination: false,
+      arrowPath: '0',
+      gap: '2rem',
+      padding: '0.5rem',
+   }).mount();
+
+   new Splide('#jugosSlider', {
+      breakpoints: {
+         450: {
+            perPage: 1,
+         },
+         600: {
+            perPage: 2,
+         },
+         800: {
+            perPage: 3,
+            gap: '1rem',
+         },
+         950: {
+            perPage: 4,
+         },
+      },
+      perPage: 5,
+      pagination: false,
+      arrowPath: '0',
+      gap: '2rem',
+      padding: '0.5rem',
+   }).mount();
 });
