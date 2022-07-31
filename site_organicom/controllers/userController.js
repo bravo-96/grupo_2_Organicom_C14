70d1ea549bf0507379f2479579df1c080138c63b
garlik05 <garlikage@protonmail.com>
2022-07-31 15:37:24 -0300

    encontrando errores

--- site_organicom/controllers/userController.js
+++ site_organicom/controllers/userController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+
 const db = require("../database/models");
 const bcrypt = require("bcryptjs");
 
@@ -67,76 +68,8 @@ module.exports = {
     db.User.findAll().then(function (users) {});
   },
 
-  // processRegister: (req, res, next) => {
-  //   let lastId = 0;
-  //   usuarios.forEach((user) => {
-  //     if (user.id > lastId) {
-  //       lastId = user.id;
-  //     }
-  //   });
-
-  //   if (
-  //     req.body.email &&
-  //     req.body.nombre &&
-  //     req.body.password !== "undefined"
-  //   ) {
-  //     let { nombre, email, number, password } = req.body;
-
-  //     let newUser = {
-  //       id: lastId + 1,
-  //       nombre,
-  //       email,
-  //       number,
-  //       password: bcrypt.hashSync(password, 10),
-  //       rol: "user",
-  //       avatar: "default.png",
-  //       terminos: true,
-  //     };
-
-  //     usuarios.push(newUser);
-  //     guardarUser(usuarios);
-
-  //     res.redirect("/");
-  //   } else {
-  //     res.redirect("/users/register");
-  //   }
-  //   next();
-
-  //   //SI HAY DUDAS, REVISAR EL ADMINCONTROLLER PARA ENTENDER COMO FUNCIONA, PARA TENER UNA REFERENCIA <3
-  //   //FALTAN COSAS POR AGREGAR PERO LAS SUBO DE A POCO
-  //   //armado de logica register Alex <3
-  // },
-  // login: (req, res, next) => {
-  //   return res.render("users/login-lateral");
-  // },
-  // processLogin: (req, res, next) => {
-  //   /* let errors = validationResult(req); */
-  //   /*  if (errors.isEmpty()) { */
-  //   let { id, nombre, email, password, avatar, rol } = usuarios.find(
-  //     (user) => user.email === req.body.email
-  //   );
-
-  //   req.session.user = {
-  //     id,
-  //     nombre,
-  //     email,
-  //     password,
-  //     avatar,
-  //     rol,
-  //   };
-
-  //   res.locals.user = req.session.user;
-  //   res.redirect("/");
-
-  //   /* } else { */
-  //   /* res.send(errors); */
-  //   /* } */
-
-  //   /* next(); */
-
-  //   //el next esta de mas
-  //   //Logica del login consultas a Alex <3
-  // },
+ 
+  
   logout: (req, res) => {
     req.session.destroy();
     res.redirect("/");
